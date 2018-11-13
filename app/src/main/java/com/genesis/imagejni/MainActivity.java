af61d13ecef46651c94a063c5e8bba1a4f0bcef4
hangwang <hangwang@ecarx.com.cn>
2018-11-13 11:21:16 +0800

    feat:add UI
    Signed-off-by: hangwang <hangwang@ecarx.com.cn>

--- app/src/main/java/com/genesis/imagejni/MainActivity.java
+++ app/src/main/java/com/genesis/imagejni/MainActivity.java
@@ -4,12 +4,20 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.genesis.imagejni.imageLib.ImageImpl;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Bitmap effectBitmap;
+    ImageView originalImageView;
+    ImageView effectImageView;
+    Button mSobel;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -17,13 +25,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Example of a call to a native method
+        mSobel = findViewById(R.id.sobel);
+        mSobel.setOnClickListener(this);
 
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.bmp_source);
-        Bitmap bitmap2 = BitmapFactory.decodeResource(getResources(), R.mipmap.bmp_boe);
-        bitmap2 = Bitmap.createScaledBitmap(bitmap2, bitmap.getWidth(), bitmap.getHeight(), true);
-
-        ImageImpl.gauss2Image(bitmap, null, 30);
-        ImageView imageView = (ImageView) findViewById(R.id.imageView);
-        imageView.setImageBitmap(bitmap);
+        Bitmap originalBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.bmp_source);
+        effectBitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.bmp_source);
+        effectBitmap = Bitmap.createScaledBitmap(effectBitmap, effectBitmap.getWidth(), effectBitmap.getHeight(), true);
+        effectImageView = (ImageView) findViewById(R.id.effect_imageView);
+        originalImageView = (ImageView) findViewById(R.id.original_imageView);
+        originalImageView.setImageBitmap(originalBitmap);
     }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("onclick",v.toString());
+        switch (v.getId()) {
+            case R.id.sobel:
+                ImageImpl.gauss2Image(effectBitmap, null, 30);
+                effectImageView.setImageBitmap(effectBitmap);
+                break;
+        }
+    }
+
+
 }
