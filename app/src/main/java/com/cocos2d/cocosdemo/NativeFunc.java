6ab8a795512af3441ec06e38f30bd81e9a1a0589
zhiling <zhiling@ecarx.com.cn>
2019-10-24 11:07:42 +0800

    feat: 配置更新

---
+++ app/src/main/java/com/cocos2d/cocosdemo/NativeFunc.java
@@ -0,0 +1,20 @@
+package com.cocos2d.cocosdemo;
+
+/**
+ * 项目名称：LibBitmap
+ * 类描述：
+ * 创建人：genesis
+ * 创建时间：2019-10-16 10:22
+ * 修改人：genesis
+ * 修改时间：2019-10-16 10:22
+ * 修改备注：
+ */
+public class NativeFunc {
+
+    public static native void nWave ();
+
+    public static native void nOpenEyes();
+
+    public static native void nTurnEyesTo(float yaw,float pitch,float rail);
+
+}
